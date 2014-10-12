77b01ba19e6e85ddea9dde6acdc578298b1ef5d6
kedz <kedzie@cs.columbia.edu>
2014-10-12 16:15:45 -0400

    Added header definitions of public functions

--- c/src/util.h
+++ c/src/util.h
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <gio/gio.h>
 
+GPtrArray *cu_slurp_dir_contents(GFile *);
+void cu_walk_dir(GFile *, 
+                 void *, 
+                 void (*) (GFile *, void *),
+                 void (*) (GFile *, void *));
 
 #define CU_NEWSTRCPY(cpy, src) char *cpy = NULL; \
     asprintf (&cpy, "%s", src);
